0cfb01169d5ffe7215db0a97919babf9dbb41138
balasnehith <balasnehith2099@gmail.com>
2024-03-18 18:19:09 -0400

    correction in previous commit

---
+++ src/Components/TextComp/HomePage/TextComp.jsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import "./textComp.css";
+import { dataConditionalRendering } from "../../../Data/Data";
+
+export default function TextComp() {
+  const [displayElement, setDisplayElement] = useState();
+
+  return (
+    <div className="tectContainer">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo consequatur tempore ipsam beatae illo error vel nemo itaque atque? Necessitatibus, enim unde soluta quas tenetur, cumque
+        consequuntur amet ut autem porro tempore. Ipsa, soluta beatae? Ex iste amet, nobis reiciendis reprehenderit beatae eius, error iusto magni aperiam minus, dolorem nesciunt quod. Molestias
+        expedita iusto odit dolore nemo, impedit perspiciatis culpa saepe unde quibusdam dolores, optio enim adipisci ea consequuntur, sint nulla? Optio placeat omnis deserunt vel. Odio optio iste
+        esse necessitatibus explicabo, eos sapiente reiciendis nesciunt debitis delectus. Necessitatibus est tempora recusandae, quis non dignissimos vel soluta ex dicta nam.
+      </p>
+      <div className="selectContainer">
+        <button onClick={() => setDisplayElement("components")}>Components</button>
+        <button onClick={() => setDisplayElement("jsx")}>JSX</button>
+        <button onClick={() => setDisplayElement("props")}>Props</button>
+        <button onClick={() => setDisplayElement("state")}>State</button>
+      </div>
+      {!displayElement && (
+        <div className="displaySelectContainer">
+          <p>Please select a topic</p>
+        </div>
+      )}
+      {displayElement && (
+        <div className="displaySelectContainer">
+          <p>{dataConditionalRendering[displayElement].title}</p>
+          <p>{dataConditionalRendering[displayElement].description}</p>
+        </div>
+      )}
+    </div>
+  );
+}
